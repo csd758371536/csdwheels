aa3d4ad5b969220e71985fff77d78f294c23a76e
csdoker <csd758371536@qq.com>
2018-03-11 22:50:26 +0800

    modify

--- src/base.js
+++ src/base.js
@@ -1,13 +1,29 @@
-;(function(window, document, undefined) {
+; // JavaScript弱语法的特点,如果前面刚好有个函数没有以";"结尾,那么可能会有语法错误
+(function(window, document, undefined) {
   'use strict';
   var Base = function() {
-
+    // 判断是用函数创建的还是用new创建的。这样我们就可以通过Base() 或 new Base()来使用这个插件了
+    if (!(this instanceof Base)) return new Base();
+    this.init();
   };
   Base.prototype = {
-
+    construct: Base,
+    init: function () {
+      
+    }
   };
   window.Base = Base;
 })(window, document);
+
+// var plugin =(function(){
+//      function _firstFunc(str){
+//          alert(str);
+//      };
+//      return{
+//          firstFunc: _firstFunc,
+//      };
+//  })();
+
 //  ;(function (root, factory) {
 //      if (typeof define === 'function' && define.amd) {
 //          // AMD
