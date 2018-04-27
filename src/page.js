1588cf086902866adb9fd219aabb0ebfc94eeddf
csdoker <csd758371536@qq.com>
2018-04-27 14:55:35 +0800

    modify

--- src/page.js
+++ src/page.js
@@ -1,5 +1,5 @@
 (function(window, document, undefined) {
-        'use strict';
+    'use strict';
 
         function addEvent(element, type, handler) {
             if (element.addEventListener) {
@@ -13,20 +13,100 @@
             }
         }
 
+        function isPlainObject(obj) {
+            var class2type = {};
+            var toString = class2type.toString;
+            var hasOwn = class2type.hasOwnProperty;
+            var proto, Ctor;
+            if (!obj || toString.call(obj) !== "[object Object]") {
+                return false;
+            }
+            proto = Object.getPrototypeOf(obj);
+            if (!proto) {
+                return true;
+            }
+            Ctor = hasOwn.call(proto, "constructor") && proto.constructor;
+            return typeof Ctor === "function" && hasOwn.toString.call(Ctor) === hasOwn.toString.call(Object);
+        }
+
+        function extend() {
+            // 默认不进行深拷贝
+            var deep = false;
+            var name, options, src, copy, clone, copyIsArray;
+            var length = arguments.length;
+            // 记录要复制的对象的下标
+            var i = 1;
+            // 第一个参数不传布尔值的情况下，target 默认是第一个参数
+            var target = arguments[0] || {};
+            // 如果第一个参数是布尔值，第二个参数是 target
+            if (typeof target == 'boolean') {
+                deep = target;
+                target = arguments[i] || {};
+                i++;
+            }
+            // 如果target不是对象，我们是无法进行复制的，所以设为 {}
+            if (typeof target !== "object" && !isFunction(target)) {
+                target = {};
+            }
+
+            // 循环遍历要复制的对象们
+            for (; i < length; i++) {
+                // 获取当前对象
+                options = arguments[i];
+                // 要求不能为空 避免 extend(a,,b) 这种情况
+                if (options != null) {
+                    for (name in options) {
+                        // 目标属性值
+                        src = target[name];
+                        // 要复制的对象的属性值
+                        copy = options[name];
+
+                        // 解决循环引用
+                        if (target === copy) {
+                            continue;
+                        }
+
+                        // 要递归的对象必须是 plainObject 或者数组
+                        if (deep && copy && (isPlainObject(copy) ||
+                                (copyIsArray = Array.isArray(copy)))) {
+                            // 要复制的对象属性值类型需要与目标属性值相同
+                            if (copyIsArray) {
+                                copyIsArray = false;
+                                clone = src && Array.isArray(src) ? src : [];
+
+                            } else {
+                                clone = src && isPlainObject(src) ? src : {};
+                            }
+
+                            target[name] = extend(deep, clone, copy);
+
+                        } else if (copy !== undefined) {
+                            target[name] = copy;
+                        }
+                    }
+                }
+            }
+
+            return target;
+        }
+
         /*
          * dataCount: 数据总数
          * pageSize: 每页最多显示的数据数量
          * pageMax: 界面最多能显示的页码数量
          */
-        var Page = function(pageOption) {
+        var Page = function(option) {
             // 支持Page()或new Page()创建
             if (!(this instanceof Page)) return new Page();
+            option = extend(true, this.defaultOption, option)
             // 最多显示页码数
-            this.pageMax = pageOption.pageMax;
+            this.pageMax = option.pageMax;
             // 当前页码
             this.pageNumber = 1;
             // 总页数
-            this.pageCount = Math.ceil(pageOption.dataCount / pageOption.pageSize);
+            this.pageCount = Math.ceil(option.dataCount / option.pageSize);
+            // 事件
+            this.pageEvent = option.pageEvent;
             // 渲染
             this.renderPages();
             // 改变页码
@@ -34,37 +114,44 @@
         };
         Page.prototype = {
                 construct: Page,
-                bindEvent: function() {
-                    if (this.pageEvent !== undefined) {
-                        this.pageEvent();
-                    }
+                // 默认配置
+                defaultOption: {
+                  dataCount: 162,
+                  pageSize: 7,
+                  pageMax: 5,
+                  pageEvent: function() {}
                 },
+                // bindEvent: function() {
+                //     if (this.pageEvent !== undefined) {
+                //         this.pageEvent();
+                //     }
+                // },
                 changePage: function() {
                     var _this = this;
                     var pagelist = document.getElementById("pagelist");
-                    this.addEvent(pagelist, 'click', function(ev) {
+                    addEvent(pagelist, 'click', function(ev) {
                         var e = ev || window.event;
                         var target = e.target || e.srcElement;
                         if (target.nodeName.toLocaleLowerCase() == 'a') {
                             if (target.id === 'prev') {
                                 _this.prevPage();
-                                _this.bindEvent();
+                                // _this.bindEvent();
                             } else if (target.id === 'next') {
                                 _this.nextPage();
-                                _this.bindEvent();
+                                // _this.bindEvent();
                             } else if (target.id === 'first') {
                                 _this.firstPage();
-                                _this.bindEvent();
+                                // _this.bindEvent();
                             } else if (target.id === 'last') {
                                 _this.lastPage();
-                                _this.bindEvent();
+                                // _this.bindEvent();
                             } else {
                                 _this.goPage(parseInt(target.innerHTML));
-                                _this.bindEvent();
+                                // _this.bindEvent();
                             }
+                            _this.pageEvent();
                         }
                     });
-                };
             },
             renderPages: function() {
                 var html = "",
