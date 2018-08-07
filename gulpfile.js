ae0d0045bc7ee6b4b075aedf940dc624be18bbae
csdoker <csd758371536@qq.com>
2018-08-07 16:25:52 +0800

    modify

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,24 @@ const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const rename = require("gulp-rename");
 
-gulp.task('page', () => {
+gulp.task('page', gulp.parallel(() => {
   return gulp.src('src/es5/pagination/pagination.js')
   .pipe(uglify())    //uglify
   .pipe(rename("pagination.min.js"))
-  .pipe(gulp.dest('dist/pagination'));
-  gulp.src('style/pagination/pagination.min.css')
   .pipe(gulp.dest('dist/pagination'))
-});
+}, () => {
+  return gulp.src('style/pagination/pagination.min.css')
+  .pipe(gulp.dest('dist/pagination'))
+}));
 
-gulp.task('mini', gulp.parallel('page'));
+gulp.task('carousel', gulp.parallel(() => {
+  return gulp.src('src/es5/carousel/carousel.js')
+  .pipe(uglify())    //uglify
+  .pipe(rename("carousel.min.js"))
+  .pipe(gulp.dest('dist/carousel'))
+}, () => {
+  return gulp.src('style/carousel/carousel.min.css')
+  .pipe(gulp.dest('dist/carousel'))
+}));
+
+gulp.task('mini', gulp.parallel('page', 'carousel'));
