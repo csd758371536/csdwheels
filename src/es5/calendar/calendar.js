0ae37d4ba3d4f85819612cca4e14e7bcc8419653
csdoker <csd758371536@qq.com>
2018-08-24 17:57:08 +0800

    calendar start

---
+++ src/es5/calendar/calendar.js
@@ -0,0 +1,53 @@
+(function(root, factory) {
+  if (typeof define === "function" && define.amd) {
+    define([], factory);
+  } else if (typeof module === "object" && module.exports) {
+    module.exports = factory();
+  } else {
+    root.Calendar = factory();
+  }
+})(typeof self !== "undefined" ? self : this, function() {
+  "use strict";
+
+  // 基于MVVM思想来实现，参考收藏的文章
+
+  // Polyfills
+  function addEvent(element, type, handler) {
+    if (element.addEventListener) {
+      element.addEventListener(type, handler, false);
+    } else if (element.attachEvent) {
+      element.attachEvent("on" + type, handler);
+    } else {
+      element["on" + type] = handler;
+    }
+  }
+
+  // 合并对象
+  function extend(o, n, override) {
+    for (var p in n) {
+      if (n.hasOwnProperty(p) && (!o.hasOwnProperty(p) || override))
+        o[p] = n[p];
+    }
+  }
+
+  var Calendar = function(selector, userOptions) {
+    // 合并配置
+    extend(this.calendarOptions, userOptions, true);
+    // 初始化
+    this.initCalendar(selector);
+    // 绑定事件
+    this.bindCalendar();
+  };
+  Calendar.prototype = {
+    calendarOptions: {},
+    initCalendar: function() {
+        this.calendar = document.querySelector(selector);
+    },
+    bindCalendar: function() {
+
+    },
+    constructor: Calendar
+  };
+
+  return Calendar;
+});
